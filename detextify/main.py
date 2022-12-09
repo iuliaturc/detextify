b6b426b52d0e111b74c00de9fc11db5b13ad77d0
Iulia <turc.raluca@gmail.com>
2022-12-09 17:50:26 -0500

    Add evaluation against annotations for images with single text box.

--- detextify/main.py
+++ detextify/main.py
@@ -3,19 +3,27 @@
 TODO: Remove this file from the library before publishing.
 """
 from absl import app
+from absl import logging
 from absl import flags
 from azure_text_detector import AzureTextDetector
 from basic_text_detector import BasicTextDetector
 
+import annotation_parser
+import cv2
+import json
 import glob
+import numpy as np
 import os
 import utils
 
 flags.DEFINE_string("input_dir", None, "Directory with input images.")
 flags.DEFINE_string("output_dir", None, "Directory with output images, with text boxes drawn on the input image.")
 flags.DEFINE_enum("mode", "BASIC", ["BASIC", "AZURE"], "Type of text detection.")
-flags.DEFINE_string("azure_endpoint", None, "Needed when --mode=AZURE. Should have the form https://<name>.cognitiveservices.azure.com/")
+flags.DEFINE_string("azure_endpoint", None,
+                    "Needed when --mode=AZURE. Should have the form https://<name>.cognitiveservices.azure.com/")
 flags.DEFINE_string("azure_key", None, "Needed when --mode=AZURE.")
+flags.DEFINE_string("annotations_file", None,
+                    "Optional path to file with ground labels. If specified, we calculate IOU against text detection.")
 flags.mark_flags_as_required(["input_dir", "output_dir"])
 FLAGS = flags.FLAGS
 
@@ -35,11 +43,42 @@ def main(_):
     if not os.path.exists(FLAGS.output_dir):
         os.makedirs(FLAGS.output_dir)
 
-    for image_path in glob.glob(os.path.join(FLAGS.input_dir, "*")):
-        text_boxes = detector.detect_text(image_path)
-        name, ext = os.path.splitext(os.path.basename(image_path))
-        output_path = os.path.join(FLAGS.output_dir, name + "_boxes" + ext)
-        utils.draw_text_boxes(text_boxes, image_path, output_path)
+    if not FLAGS.annotations_file:
+        # Simply draw the detected text boxes.
+        for image_path in glob.glob(os.path.join(FLAGS.input_dir, "*")):
+            text_boxes = detector.detect_text(image_path)
+            name, ext = os.path.splitext(os.path.basename(image_path))
+            output_path = os.path.join(FLAGS.output_dir, name + f"_boxes_{FLAGS.mode}" + ext)
+            utils.draw_text_boxes(text_boxes, image_path, output_path)
+        return
+
+    # Evaluate detected text boxes against gold annotations.
+    ious = []
+    with open(FLAGS.annotations_file) as f:
+        annotations = json.load(f)
+        for annotation in annotations:
+            image_basename = annotation_parser.get_image_basename(annotation)
+            image_path = os.path.join(FLAGS.input_dir, image_basename)
+            if not os.path.exists(image_path):
+                logging.error(f"Unable to find image {image_path}.")
+                continue
+            detected_boxes = detector.detect_text(image_path)
+            golden_boxes = annotation_parser.convert_to_text_boxes(annotation)
+
+            if len(detected_boxes) != 1 or len(golden_boxes) != 1:
+                # TODO(julia): Figure out how to handle multiple text boxes.
+                continue
+
+            ious.append(utils.intersection_over_union(detected_boxes[0], golden_boxes[0]))
+
+            # Draw the boxes so that they can be inspected visually.
+            image = cv2.imread(image_path)  # (B, G, R)
+            utils.draw_text_box(golden_boxes[0], image, color=(0, 255, 0))
+            utils.draw_text_box(detected_boxes[0], image, color=(0, 0, 255))
+            output_path = os.path.join(FLAGS.output_dir, image_basename)
+            cv2.imwrite(output_path, image)
+
+    print("Average IOU across %d images: %f" % (len(ious), np.mean(ious)))
 
 
 if __name__ == "__main__":
--- detextify/utils.py
+++ detextify/utils.py
@@ -6,14 +6,29 @@ import cv2
 import numpy as np
 
 
-def draw_text_box(tb: TextBox, image: np.ndarray):
+def draw_text_box(tb: TextBox, image: np.ndarray, color=(0, 0, 255)):
     """Draws a red rectangle around the text box. Modifies the array in place."""
-    cv2.rectangle(image, (tb.x, tb.y), (tb.x + tb.h, tb.y + tb.w), (0, 0, 255), 2)
+    cv2.rectangle(image, (tb.x, tb.y), (tb.x + tb.h, tb.y + tb.w), color, 2)
 
 
-def draw_text_boxes(tbs: Sequence[TextBox], in_path: str, out_path: str):
+def draw_text_boxes(tbs: Sequence[TextBox], in_path: str, out_path: str, color=(0, 0, 255)):
     """Draws red rectangles around the given text boxes."""
     image = cv2.imread(in_path)
     for tb in tbs:
-        draw_text_box(tb, image)
+        draw_text_box(tb, image, color)
     cv2.imwrite(out_path, image)
+
+
+def intersection_over_union(box1: TextBox, box2: TextBox):
+    # Determine the (x, y)-coordinates of the intersection rectangle.
+    xa = max(box1.x, box2.x)
+    ya = max(box1.y, box2.y)
+    xb = min(box1.x + box1.h, box2.x + box2.h)
+    yb = min(box1.y + box1.w, box2.y + box2.w)
+    # Compute the area of intersection rectangle.
+    intersection_area = max(0, xb - xa + 1) * max(0, yb - ya + 1)
+    # Compute the area of both the prediction and ground-truth rectangles
+    box1_area = (box1.h + 1) * (box1.w + 1)
+    box2_area = (box2.h + 1) * (box2.w + 1)
+    iou = intersection_area / float(box1_area + box2_area - intersection_area)
+    return iou
