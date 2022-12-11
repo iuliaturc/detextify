832036474fb5c87102c5e38ff43ae6fd23ca4c72
Iulia <turc.raluca@gmail.com>
2022-12-11 18:36:28 -0500

    Add Replicate Inpainter

--- detextify/inpainter.py
+++ detextify/inpainter.py
@@ -1,7 +1,9 @@
 """In-painting models."""
 import io
 import openai
+import replicate
 import requests
+import tempfile
 
 from PIL import Image, ImageDraw
 from text_detector import TextBox
@@ -13,7 +15,7 @@ class Inpainter:
   # TODO(julia): Run some experiments to determine the best prompt.
   PROMPT = "plain background"
 
-  def inpaint(self, image_path: str, text_boxes: Sequence[TextBox], out_image_path: str) -> str:
+  def inpaint(self, in_image_path: str, text_boxes: Sequence[TextBox], out_image_path: str):
     pass
 
 
@@ -25,6 +27,7 @@ class DalleInpainter(Inpainter):
 
   @staticmethod
   def _make_mask(text_boxes: Sequence[TextBox], height: int, width: int) -> bytes:
+    """Returns an .png where the text boxes are transparent."""
     mask = Image.new("RGBA", (width, height), (0, 0, 0, 1))  # fully opaque
     mask_draw = ImageDraw.Draw(mask)
     for text_box in text_boxes:
@@ -52,18 +55,42 @@ class DalleInpainter(Inpainter):
 
 
 class ReplicateInpainter(Inpainter):
-  def __init__(self, replicate_key: str):
-    self.replicate_key = replicate_key
+  SD_INPAINTING_V15 = "andreasjansson/stable-diffusion-inpainting"
+  SD_INPAINTING_V15_VERSION = "8eb2da8345bee796efcd925573f077e36ed5fb4ea3ba240ef70c23cf33f0d848"
 
-  def inpaint(self, image_path: str, text_boxes: Sequence[TextBox], out_image_path: str) -> str:
-    # TODO(julia): Implement this.
-    raise NotImplemented()
+  def __init__(self, replicate_token: str, model_name=SD_INPAINTING_V15, model_version=SD_INPAINTING_V15_VERSION):
+    replicate_client = replicate.Client(api_token=replicate_token)
+    self.model = replicate_client.models.get(model_name).versions.get(model_version)
+
+  @staticmethod
+  def _make_mask(text_boxes: Sequence[TextBox], height: int, width: int, out_mask_path: str):
+    """Returns a black image with white rectangles where the text boxes are."""
+    mask = Image.new("RGB", (width, height), (0, 0, 0))  # black
+    mask_draw = ImageDraw.Draw(mask)
+    for text_box in text_boxes:
+      mask_draw.rectangle(xy=(text_box.x, text_box.y, text_box.x + text_box.h, text_box.y + text_box.w),
+                          fill=(255, 255, 255))  # white
+    mask.save(out_mask_path)
+
+  def inpaint(self, in_image_path: str, text_boxes: Sequence[TextBox], out_image_path: str):
+    image = Image.open(in_image_path)  # open the image to inspect its size
+    mask_temp_file = tempfile.NamedTemporaryFile(suffix=".jpeg")
+    self._make_mask(text_boxes, image.height, image.width, mask_temp_file.name)
+
+    url = self.model.predict(prompt=Inpainter.PROMPT,
+                             image=open(in_image_path, "rb"),
+                             mask=open(mask_temp_file, "rb"),
+                             num_outputs=1)[0]
+
+    out_image_data = requests.get(url).content
+    out_image = Image.open(io.BytesIO(out_image_data))
+    out_image.save(out_image_path)
 
 
 class LocalInpainter(Inpainter):
   def __init__(self, inpainting_model_path: str):
     self.inpainting_model_path = inpainting_model_path
 
-  def inpaint(self, image_path: str, text_boxes: Sequence[TextBox], out_image_path: str) -> str:
+  def inpaint(self, in_image_path: str, text_boxes: Sequence[TextBox], out_image_path: str):
     # TODO(julia): Implement this.
     raise NotImplemented()
